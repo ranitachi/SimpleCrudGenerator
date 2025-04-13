bd1326768b19bdf3b428824b2129ba16b93e5a86
Fachran Nazarullah <fachran.nazarullah@gmail.com>
2025-04-13 16:21:35 +0700

    update service provider

--- src/SimpleCrudGeneratorServiceProvider.php
+++ src/SimpleCrudGeneratorServiceProvider.php
@@ -15,17 +15,14 @@ class SimpleCrudGeneratorServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Load default blade view stub path (for render fallback)
-        $this->loadViewsFrom(__DIR__.'/../resources/views/stubs/blade', 'simple-crud');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views/stubs/blade', 'simple-crud');
 
-        // Publish views to vendor for customization
         $this->publishes([
-            __DIR__.'/../resources/views/stubs/blade' => resource_path('views/vendor/simple-crud'),
+            __DIR__.'/../../resources/views/stubs/blade' => resource_path('views/vendor/simple-crud'),
         ], 'simple-crud-views');
 
-        // Optional: publish stubs for customization
         $this->publishes([
-            __DIR__.'/../Stubs' => base_path('stubs/simple-crud'),
+            __DIR__.'/../../Stubs' => base_path('stubs/simple-crud'),
         ], 'simple-crud-stubs');
     }
 }
